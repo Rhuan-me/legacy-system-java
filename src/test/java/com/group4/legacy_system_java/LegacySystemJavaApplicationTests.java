d26829c329573339ca14a49aebd89a6500dd4d4c
Rhuan Miguel Esteves <laterhuanme@gmail.com>
2025-09-22 21:10:41 -0300

    RME/updatePipeApplication

--- src/test/java/com/group4/legacy_system_java/LegacySystemJavaApplicationTests.java
+++ src/test/java/com/group4/legacy_system_java/LegacySystemJavaApplicationTests.java
@@ -2,12 +2,13 @@ package com.group4.legacy_system_java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class LegacySystemJavaApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
